3b076d4a2c26f221b481d4f54cb31c9bad351665
LasinduChamika <lasinduchamika@gmail.com>
2025-10-19 16:06:04 +0530

    Add files via upload

---
+++ src/assets/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-image"src="download.jpg" alt="profile picture"></img>
+            <h2 className="card-title">Bro Code</h2>
+            <p className="card-text">I make Youtube videos and play games</p>
+        </div>
+    )
+
+}
+export default Card
